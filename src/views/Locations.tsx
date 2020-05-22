2d6e87839460c893dad45d1ae897815bb143ae39
Joona Uutela <joona.uutela@gmail.com>
2020-05-22 16:12:38 +0300

    Started working on map markers fetched from API.

--- src/views/Locations.tsx
+++ src/views/Locations.tsx
@@ -1,27 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import { Row, Col } from 'antd';
+import axios from 'axios';
 
 const Locations: React.FC = () => {
 
+    const [latitude, setLatitude] = useState(0);
+    const [longitude, setLongitude] = useState(0);
+
+    React.useEffect(() => {
+        const fetchPatientList = async () => {
+            const country = await axios.get('https://restcountries.eu/rest/v2/name/sweden');
+            console.log(country.data[0].latlng[0]);
+            setLatitude(country.data[0].latlng[0]);
+            setLongitude(country.data[0].latlng[1]);
+        };
+        fetchPatientList();
+    }, []);
+
     return (
         <div className="content">
-
-
             <div className="map-container">
-                <Map center={[51.505, -0.09]} zoom={13}>
+                <Map center={[latitude, longitude]} zoom={13}>
                     <TileLayer
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                     />
-
+                    <Marker position={[latitude, longitude]}>
+                        <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+                    </Marker>
                 </Map>
             </div>
             <Row>
-                <Col span={12}>
+                <Col span={14}>
                     <div className="list-container">
-                        <h1>Ulkoliikuntapaikat</h1>
+                        <h1>Ulkoliikuntalaitteet</h1>
                         <ul className="list">
                             <li className="list-item">Kupittaa</li>
                             <li className="list-item">Kupittaa</li>
@@ -37,13 +51,15 @@ const Locations: React.FC = () => {
                         </ul>
                     </div>
                 </Col>
-                <Col span={12}>
+                <Col span={10}>
                     <div className="selected-item-container">
-                        <h1>Valittu paikka</h1>
-                        <p>Nimi</p>
-                        <p>Nimi</p>
-                        <p>Nimi</p>
-                        <p>Nimi</p>
+                        <h1>Valittu sijainti</h1>
+                        <div className="info-box">
+                            <p className="info-item"><b>Osoite:</b> Lauklähteenkatu 13 Turku</p>
+                            <p className="info-item"><b>Laitteet: </b>Ylätalja, punnerruslaite, dippitanko</p>
+                            <p className="info-item">Katso kuvat täältä</p>
+
+                        </div>
                     </div>
                 </Col>
             </Row>
